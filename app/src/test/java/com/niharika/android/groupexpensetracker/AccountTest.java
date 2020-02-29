1d98dbc4d8a00a12dbced0e61d47f479a8861f62
niharika <you@example.com>
2020-02-29 13:24:30 +0530

    Added Unit Test Account

---
+++ app/src/test/java/com/niharika/android/groupexpensetracker/AccountTest.java
@@ -0,0 +1,25 @@
+package com.niharika.android.groupexpensetracker;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(JUnit4.class)
+public class AccountTest {
+    private Account mAccount;
+
+    @Before
+    public void setUp(){
+       mAccount=new Account();
+    }
+    @Test
+    public void accountShouldNotBeDefaultAccountUntilSet() {
+        boolean resultAccount=mAccount.isDefaultAccount();
+
+        assertThat(resultAccount,is(equalTo(false)));
+    }
+}
